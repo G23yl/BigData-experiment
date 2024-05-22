9384244498086a175d3b86b9c7c2631d4363e081
gyl <1265126315@qq.com>
2024-05-22 18:03:56 +0800

    second commit

--- experiment2/pagerank.py
+++ experiment2/pagerank.py
@@ -1,4 +1,6 @@
 import numpy as np
+import pandas as pd
+
 
 def readfile(file_name: str):
     relation_dict = {}
@@ -9,40 +11,46 @@ def readfile(file_name: str):
         key_word = line.strip().split(":")[0]
         link_to = line.strip().split(":")[1]
         key_words.append(key_word)
-        for word in link_to.split(" "):
-            temp[word] = 1
+        if link_to != "":
+            for word in link_to.split(" "):
+                temp[word] = 1
         relation_dict[key_word] = temp
-    return key_words, relation_dict
+    return sorted(key_words), relation_dict
 
-def PageRank(M, beita = 0.8):
-    page_rank_init = np.array(np.ones(M.shape[0]).T / M.shape[0])
-    page_rank = page_rank_init
-    beita_M = M * beita
-    bias = page_rank_init * (1 - beita)
+
+def PageRank(M, beita=0.8):
+    N = M.shape[0]
+    r0 = np.full((N, 1), 1 / N, dtype=float)
+    bias = (1 - beita) / N * np.ones((N, 1), dtype=float)
     while True:
-        page_rank = np.dot(beita_M, page_rank_init) + bias
-        error = np.sum(np.abs(page_rank - page_rank_init))
-        if error < 1e-8:
+        print(np.sum(r0))
+        temp = r0
+        r0 = beita * np.dot(M, r0) + bias
+        error = np.sum(np.abs(temp - r0))
+        if error < 1e-08:
             break
-        page_rank_init = page_rank
-    return page_rank
+    return r0
 
 
 if __name__ == "__main__":
     (key_words, relation_dict) = readfile("./link_relation.txt")
-    matrix = []
-    for key in key_words:
-        arr = []
-        link_to = relation_dict[key]
-        rank = len(link_to.keys())
-        for i in range(len(key_words)):
-            if link_to.get(key_words[i]) != None:
-                arr.append(1 / rank)
-            else:
-                arr.append(0)
-        matrix.append(arr)
-    link_matrix = np.array(matrix).T
-    page_rank = PageRank(link_matrix)   
+    matrix = pd.DataFrame(np.zeros((len(key_words), len(key_words))), index=key_words, columns=key_words, dtype=float)
+    # matrix = np.zeros((1000, 1000), dtype=float)
+    for i in range(len(key_words)):
+        link_to = relation_dict[key_words[i]]
+        for j in range(len(key_words)):
+            if link_to.get(key_words[j]) != None:
+                matrix[key_words[j]][key_words[i]] = 1
+                # matrix.loc[key_words[j], key_words[i]] = 1.0
+    matrix = matrix.div(matrix.sum(axis=0), axis=1)
+    matrix = matrix.fillna(0)
+    # for i in range(len(key_words)):
+    #     sum = np.sum(matrix[i])
+    #     if sum != 0:
+    #         matrix[i] /= sum
+    # matrix = matrix.T
+    page_rank = PageRank(matrix)
+    print(np.sum(page_rank))
     with open("./page_rank.txt", "w") as f:
         for i in range(len(key_words)):
-            f.write("{}: {}\n".format(key_words[i], page_rank[i]))
+            f.write(f"{key_words[i]}: {page_rank[i][0]}\n")
